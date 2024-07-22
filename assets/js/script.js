edc60bea9e4b95fc1436c2e91bcd6ff1f69ed2b6
Samuel Santos <71991685+Samuelsts@users.noreply.github.com>
2024-07-22 14:33:09 -0300

    adicionado carrossel da area tec. e remoção de cod nao utilizados

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+
+// scroll up
 window.addEventListener('scroll', function(){
     let scroll = document.querySelector('.scrolltop')
     scroll.classList.toggle('active', window.scrollY > 450)
@@ -16,3 +18,8 @@ document.addEventListener('DOMContentLoaded', function() {
     const scrollButton = document.querySelector('.scrolltop');
     scrollButton.addEventListener('click', backTop);
 });
+
+// duplicação do slide tec
+
+let  clone= document.querySelector(".logos-slide").cloneNode(true)
+    document.querySelector(".logos").appendChild(clone);
