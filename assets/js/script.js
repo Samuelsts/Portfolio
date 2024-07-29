07462cdcc7e5c42b54b8794a5b6a65e0ef955d79
Samuel Santos <71991685+Samuelsts@users.noreply.github.com>
2024-07-28 23:15:51 -0300

    Adicionado seção serviços

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,5 +21,16 @@ document.addEventListener('DOMContentLoaded', function() {
 
 // duplicação do slide tec
 
-let  clone= document.querySelector(".logos-slide").cloneNode(true)
+document.addEventListener("DOMContentLoaded", function() {
+    // Clona a div logos-slide
+    let clone = document.querySelector(".logos-slide").cloneNode(true);
     document.querySelector(".logos").appendChild(clone);
+
+    // Força o recálculo da largura antes de iniciar a animação
+    const logosSlide = document.querySelectorAll(".logos-slide");
+    logosSlide.forEach(slide => {
+        slide.style.animation = "none";
+        slide.offsetHeight; // Força o recálculo
+        slide.style.animation = "";
+    });
+});
