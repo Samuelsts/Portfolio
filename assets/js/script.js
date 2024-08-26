6199acae16b3f5c9bebee5261d60dee0b5f756db
Samuel Santos <71991685+Samuelsts@users.noreply.github.com>
2024-08-26 11:18:45 -0300

    Criação do carrossel e adaptação responsiva

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,3 +34,127 @@ document.addEventListener("DOMContentLoaded", function() {
         slide.style.animation = "";
     });
 });
+
+
+// Menu Mobile
+const btnMenu= document.querySelector('.btn-abrir');
+const menu= document.querySelector('.menu-mob');
+const overlay= document.querySelector('.overlay-menu-mob');
+
+btnMenu.addEventListener('click',()=>{
+    menu.classList.add('abr-mb')
+});
+
+menu.addEventListener('click',()=>{
+    menu.classList.remove('abr-mb')
+});
+
+overlay.addEventListener('click',()=>{
+    menu.classList.remove('abr-mb')
+});
+
+// classe active menu desktop
+const iMenu=document.querySelectorAll('.m-item');
+
+const remActive = ()=> {
+    iMenu.forEach(i=>i.classList.remove('active'));
+};
+
+iMenu.forEach(i=>{
+    i.addEventListener('click', ()=>{
+        remActive();
+        i.classList.add('active')
+    });
+});
+
+// Projetos
+
+const box = document.querySelector('.container');
+const cards = document.querySelectorAll('.card-proj');
+const btnNex = document.querySelector('.btn-ir');
+const btnPre = document.querySelector('.btn-voltar');
+
+let intervalo;
+let contador = 0;
+
+function avancarSlide() {
+            contador++;
+            if (contador > cards.length - 1) {
+                contador = 0;
+            }
+            atualizarSlider();
+        }
+
+        function voltarSlide() {
+            contador--;
+            if (contador < 0) {
+                contador = cards.length - 1;
+            }
+            atualizarSlider();
+        }
+
+        function atualizarSlider() {
+            box.style.transform = `translateX(${-contador * 400}px)`;
+        }
+
+        function iniIntervalo() {
+            intervalo = setInterval(avancarSlide, 2000);
+        }
+
+        function paraIntervalo() {
+            clearInterval(intervalo);
+        }
+
+        btnNex.addEventListener('click', () => {
+            paraIntervalo();
+            avancarSlide();
+            iniIntervalo();
+        });
+
+        btnPre.addEventListener('click', () => {
+            paraIntervalo();
+            voltarSlide();
+            iniIntervalo();
+        });
+
+        cards.forEach(card => {
+            card.addEventListener("mouseover", paraIntervalo);
+            card.addEventListener("mouseout", iniIntervalo);
+        });
+
+        iniIntervalo();
+
+// const box =document.querySelector('.container');
+// const cards= document.querySelectorAll('.card-proj');
+// const btnNex= document.querySelector('btn-ir');
+// const btnPre= document.querySelector('btn-voltar');
+
+// let intervalo
+// let contador= 0;
+// function slider (){
+//     contador++;
+//     if(contador > cards.length -1){
+//         contador=0;
+//     }
+//     box.style.transform =`translateX(${-contador * 400}px)`;
+// }
+
+// btnNex.addEventListener('click',()=>{
+//     if(contador > cards.length -1){
+//         contador=0;
+//     }
+//     box.style.transform =`translateX(${-contador * 400}px)`;
+// })
+
+// function iniIntervalo(){
+//    intervalo =setInterval(slider,2000);
+// }
+// function paraIntervalo(){
+//     clearInterval(intervalo)
+// }
+// iniIntervalo()
+
+// cards.forEach(card=>{
+//     card.addEventListener("mouseover",paraIntervalo)
+//     card.addEventListener("mouseout",iniIntervalo)
+// })
